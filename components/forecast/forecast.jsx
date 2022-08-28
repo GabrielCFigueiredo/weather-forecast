45c4ea9433ed07c09ad70df310548a0c6078f6bd
Gabriel Figueiredo <gabriel.devsistema@gmail.com>
2022-08-28 20:28:54 -0300

    update

--- components/forecast/forecast.jsx
+++ components/forecast/forecast.jsx
@@ -56,7 +56,7 @@ export default function Forecast() {
           height: "80vh",
         }}
       >
-        <Box sx={{ height: "500px"}}>
+        <Box>
           <Box
             sx={{
               height: "60vh",
@@ -112,7 +112,7 @@ export default function Forecast() {
             opacity: "0.6",
             height: "170px",
             color: "white",
-            marginTop: "120px",
+            marginTop: "50px",
           }}
         >
           <Box
